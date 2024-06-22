c6badbeb4fed4813d907e85de7432d04ea71eeb7
Prabal <aryalparbal@gmail.com>
2024-06-22 13:53:49 +0545

    functionality with which user can search for video,pick there choices and download them has been added

--- video_download.py
+++ video_download.py
@@ -1,38 +1,39 @@
 from pytube import YouTube
+from pytube import Search
+from pytube import __title__
 import pandas as pd
-import os
-import csv
 
 
-def download_video(url, path):
-    vid = YouTube(url)
-    video = vid.streams.get_highest_resolution()
-    if not os.path.exists(path):
-        os.makedirs(path)
-    if vid == None:
-        print("The video does not exist")
+def search_results(video_name):
+    # searching for the video in youtube
+    result = Search(video_name)
+    print(f"{len(result.results)} have been found ")
+    # storing the video and link in a dictionary
+    video_dict = {"video": [], "link": []}
+    i = 1
+    for video in result.results:
+        print(f"{i} {video.title} - {video.watch_url}\n")
+        video_dict["video"].append(video.title)
+        video_dict["link"].append(video.watch_url)
+        i += 1
+    # to allow the user to choose which video to download from the search results
+    print("\n\n Enter the number of the video you want to download")
+    number = int(input())
+    video_download(number, video_dict)
+    return
 
-    video_info = {"title": vid.title, "url": url}
-    df = pd.DataFrame(video_info, index=False)
-    df.to_csv("video_info.csv", mode="a", header=False)
 
-    check_video_downloaded("url")
-    print("Video has been downloaded")
-
-
-def check_video_downloaded(url, path):
-    df = pd.read_csv("video_info.csv")
-    length = len(df["url"])
-    for i in length:
-        if df["url"][i] == url:
-            return
-    download_video(url, path)
+# for downloading the video in best possible resolution
+def video_download(number, video_dict):
+    video = YouTube(video_dict["link"][number - 1])
+    print("Downloading................. ")
+    video.streams.get_highest_resolution().download()
+    print("download complete.................")
 
 
 def main():
-    url = input("Enter the URL of the video: ")
-    path = "E:/YouTube-Video-Downloader/videos"
-    download_video(url, path)
+    video_name = "sakkigoni"
+    search_results(video_name)
 
 
 if __name__ == "__main__":
